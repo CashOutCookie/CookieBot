0141e1d0f98e1bb8fd6a2525e00208906d2e407e
StingBolt <stingbolt1@gmail.com>
2021-03-11 23:48:43 +0530

    Done

--- bot.py
+++ bot.py
@@ -10,14 +10,15 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="https://cashoutcookie.com"))
     print("I'm Ready!")
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-  
-@bot.event
 async def on_message(message):
     if str(message.channel) == '📃todo' and not message.content.startswith("TO-DO"):
         await message.delete()
 
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
 token = os.environ.get('TOKEN')
-bot.run(token)
+bot.run(token) 
