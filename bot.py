f1db2c1dc550ec87e4eed82a43aeaad0afab0931
StingBolt <stingbolt1@gmail.com>
2021-03-11 23:24:06 +0530

    Preifx/Req.txt

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 from decouple import config
 
-bot = commands.Bot(command_prefix = '!')
+bot = commands.Bot(command_prefix = '?')
 bot.remove_command('help')
 
 @bot.event
