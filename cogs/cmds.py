7223fb4202be7d8a1437c73c7cbcb3cf3effbde1
StingBolt <stingbolt1@gmail.com>
2021-03-12 20:22:07 +0530

    emergency ping

--- cogs/cmds.py
+++ cogs/cmds.py
@@ -14,6 +14,7 @@ class Commands(commands.Cog):
         embed.add_field(name="?source", value="My Github source code!")
         embed.add_field(name="?leaderboard", value="Leaderboard for [CashoutCookie](https://cashoutcookie.com/rank)!")
         embed.add_field(name="?profile `<username>`", value="User profile from [CashoutCookie](https://cashoutcookie.com)!", inline=False)
+        embed.add_field(name="?emergency", value="Use this command **only** when there is a huge bug/mistake or any major problem, if it's some small bug or issue then directly ping @team", inline=False)
 
         await ctx.send(embed=embed)
 
@@ -23,5 +24,17 @@ class Commands(commands.Cog):
         embed.set_thumbnail(url="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png")
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def emergency(self, ctx):
+        modchannel = self.bot.get_channel(812560533428502531)
+        embed = discord.Embed(title="Spam Ping Started..", description=f"Busy spam pinging the team in DMs for your help {ctx.author.name}", color=discord.Color.orange())
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/807140294764003350/818505400449761351/cookiemoney.png")
+        await ctx.send(embed=embed)
+        await modchannel.send("EMERGENCY SLOTHS! SOMEONE NEEDS YOUR HELP <@&819944514558492673>")
+        await modchannel.send(f"<@&819944514558492673> user which used the command is {ctx.author.name} btw")
+        await modchannel.send(f"<@&819944514558492673> this is their id {ctx.author.id}")
+        await modchannel.send(f"<@&819944514558492673> Help them!")
+        await modchannel.send("<@&819944514558492673>")
+        await modchannel.send("<@&819944514558492673>")
 def setup(bot):
     bot.add_cog(Commands(bot))
