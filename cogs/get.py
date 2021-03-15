4cc61fd8c0c6a246726c14ad70a8137a2d748829
StingBolt <74103155+StingBolt@users.noreply.github.com>
2021-03-15 15:48:55 +0530

    Typo

--- cogs/get.py
+++ cogs/get.py
@@ -8,7 +8,7 @@ class GetAPIData(commands.Cog):
         
     @commands.command()
     async def leaderboard(self, ctx):
-        URL = "https://api.cashoutcookie.conm/api/leaderboard/"
+        URL = "https://api.cashoutcookie.com/api/leaderboard/"
         embed = discord.Embed(title="CashOutCookie Leaderboard",
                              description=f"[Check the full leaderboard here](https://cashoutcookie.com/rank)",
                              color=discord.Colour.teal())
