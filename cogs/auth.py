17d9894ad0f995e690a996474423f8d96bbbf8a7
StingBolt <stingbolt1@gmail.com>
2021-03-20 09:20:30 +0530

    'exception

--- cogs/auth.py
+++ cogs/auth.py
@@ -23,22 +23,24 @@ class Auth(commands.Cog):
                 msg = await self.bot.wait_for('message', check=check, timeout=60.0)
             except asyncio.TimeoutError:
                 await ctx.author.send("You took too long to login, try again by using the same command (`?login`)")
-            if str(msg.content.startswith("login")):
+            try:
+                if str(msg.content.startswith("login")):
 
-                URL = "http://localhost:8000/api/botlogin/"
-                data = {"username": msg.content.split(" ")[1], "password": msg.content.split(" ")[2]}
-                credentials = json.dumps(data)
-                await ctx.author.send("Trying to log in with the credentials provided...")
-                async with request("POST", URL, data=credentials, headers={'Content-type':'application/json', 'Accept':'application/json'}) as response:
-                    if response.status == 202:
-                        self.bot.listcookies.append((msg.content.split(" ")[1], ctx.author.id))
-                        await ctx.author.send(embed=discord.Embed(title="Successfully logged in to CashOut Cookie!", color=discord.Colour.green()))
-                    elif response.status == 406:
-                        await ctx.author.send(embed=discord.Embed(title="Wrong Password", description="The password you entered is invalid, try again with the same command (`?login`)", color=discord.Color.orange()))
-                    else:
-                        await ctx.author.send(embed=discord.Embed(title="Username or Password are invalid", description="The username and password combination you entered is invalid, try again with the same command (`?login`)", color=discord.Color.red()))
+                    URL = "http://localhost:8000/api/botlogin/"
+                    data = {"username": msg.content.split(" ")[1], "password": msg.content.split(" ")[2]}
+                    credentials = json.dumps(data)
+                    await ctx.author.send("Trying to log in with the credentials provided...")
+                    async with request("POST", URL, data=credentials, headers={'Content-type':'application/json', 'Accept':'application/json'}) as response:
+                        if response.status == 202:
+                            self.bot.listcookies.append((msg.content.split(" ")[1], ctx.author.id))
+                            await ctx.author.send(embed=discord.Embed(title="Successfully logged in to CashOut Cookie!", color=discord.Colour.green()))
+                        elif response.status == 406:
+                            await ctx.author.send(embed=discord.Embed(title="Wrong Password", description="The password you entered is invalid, try again with the same command (`?login`)", color=discord.Color.orange()))
+                        else:
+                            await ctx.author.send(embed=discord.Embed(title="Username or Password are invalid", description="The username and password combination you entered is invalid, try again with the same command (`?login`)", color=discord.Color.red()))
 
-
+            except:
+                await ctx.author.send("You need to use 'login' in the beginning of your message,\n Example: ```login myepicusername mysecretpassword123``` Try again using `?login`.")
 def setup(bot): 
     bot.add_cog(Auth(bot))
 
